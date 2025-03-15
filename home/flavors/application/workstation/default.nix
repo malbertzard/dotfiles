d1ec57094e1588afb1b88dfb46c7e6b5fe091c36
Mathis Albertzard <malbertzard@gmail.com>
2025-03-15 23:24:01 +0100

    chore: Remove vscode

--- home/flavors/application/workstation/default.nix
+++ home/flavors/application/workstation/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ../../../../overlays/emacs.nix
     ../../../../home/features/application/emacs.nix
-    ../../../../home/features/application/vscode.nix
   ];
 
   home.packages =
