093148d953738bbc0f0553a4619143e54a7dd330
malbertzard <malbertzard@gmail.com>
2025-10-23 22:59:05 +0200

    chore: Some emacs stuff

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -1,4 +1,4 @@
 (import (builtins.fetchTarball {
   url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-  sha256 = "sha256:101k1kb519f3qp5a1whr96k9qxlb1rfda714fkvwc4qscnffcl3r";
+  sha256 = "sha256:1rk17lhlsilf51c6jrs4hw0l2jvs8l97l1wqah2q8glppywndn89";
 }))
