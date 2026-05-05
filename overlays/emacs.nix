484ea9f85a5d5d0d81d803cfb6b0e8c6eb7187e5
malbertzard <malbertzard@gmail.com>
2026-05-05 23:45:29 +0200

    chore: Update overlays

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -1,4 +1,4 @@
 (import (builtins.fetchTarball {
   url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-  sha256 = "sha256:1xfm0k89981kh6qbqnn0n884s65r46pb3jkaf88h6jn6vbka54ws";
+  sha256 = "sha256:0zmaa40l0nz4dxpc9203880c5yh40lagflw4sqcrjkpxlwnr75vm";
 }))
--- overlays/rust.nix
+++ overlays/rust.nix
@@ -1,5 +1,5 @@
 (import (builtins.fetchTarball {
   url = "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
-  sha256 = "sha256:1crbnp43q5pki1mhjs842lfc088g3j3b3ahn9xvc5gxcikmin1hp";
+  sha256 = "sha256:0q0835cdz332w6dkcpyilxwf5xhmy13kaqwn6k1p8s0jqavs14rn";
 }))
 
